658a1561db7d9e5585f056064037d21b57cbec4b
Abhishek Verma <avishekv444@gmail.com>
2021-08-28 00:54:09 +0530

    Added Store

---
+++ src/reducers/posts.js
@@ -0,0 +1,4 @@
+export default function posts (state=[]){
+    //current state will look like this,we will have a object and this we will have posts Araay for now:-{ posts:[] }
+    return state;
+}
---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, createStore } from "redux";
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import reducer from '../reducers/index'; //if you dont mention index then also it will work
+
+
+let store;
+
+export function configureStore(){
+    store=createStore(reducer,applyMiddleware(thunk,logger));
+    return store;
+
+}
