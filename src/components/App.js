4151818d12e9331d2ee851df5d9ebda4b51acac1
Abhishek Verma <avishekv444@gmail.com>
2021-08-29 01:21:21 +0530

    add react-router 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,73 +1,52 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import  {BrowserRouter as Router,Link,Route } from 'react-router-dom'; //renaming BrowserRouter as Router for us
 import PropTypes from 'prop-types';
 
 import { fetchPosts } from '../actions/posts';
-import { PostsList } from './'; //automatically importing from index.js
+import { PostsList,Navbar } from './'; //automatically importing from index.js
+
+//all 3 are dummy components ,for learning porpose
+const Login= () => <div> Login</div>
+const SignUp= () => <div> SignUp</div> 
+const Home= () =><div> Home</div> 
+
 
 class App extends React.Component {
   componentDidMount() {
     this.props.dispatch(fetchPosts());
   }
-
+ 
+  //we have to wrap everything between router:-this will basically tell react router that hey this is our root application
   render() {
     const { posts } = this.props;
     return (
-      <div>
-        <nav className="nav">
-          <div className="left-div">
-            <img
-              src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
-              alt="logo"
-            />
-          </div>
-          <div className="search-container">
-            <img
-              className="search-icon"
-              src="https://image.flaticon.com/icons/svg/483/483356.svg"
-              alt="search-icon"
-            />
-            <input placeholder="Search" />
+      <Router> 
+        <div>
+          <Navbar />
+          {/* <PostsList posts={posts} /> */}
+          <ul>
+            <li>
+              <Link to='/'>
+                {/* link tag internally usses <a> tag only but <a> tag gives reloading while loading href and link tag dosent do this */}
+                Home
+              </Link>           
+            </li>
+            <li>
+            <Link to='/login'>Login</Link>
+            </li>
+            <li>
+            <Link to='/signup'>SignUp</Link>   
+            </li>
+          </ul>
 
-            <div className="search-results">
-              <ul>
-                <li className="search-results-row">
-                  <img
-                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
-                    alt="user-dp"
-                  />
-                  <span>John Doe</span>
-                </li>
-                <li className="search-results-row">
-                  <img
-                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
-                    alt="user-dp"
-                  />
-                  <span>John Doe</span>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="right-nav">
-            <div className="user">
-              <img
-                src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
-                alt="user-dp"
-                id="user-dp"
-              />
-              <span>John Doe</span>
-            </div>
-            <div className="nav-links">
-              <ul>
-                <li>Log in</li>
-                <li>Log out</li>
-                <li>Register</li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-        <PostsList posts={posts} />
-      </div>
+           {/* whatever above this lines are will be common if we route to any component */}
+          <Route exact path="/" component={Home} /> 
+          {/* this means if my path becomes exactly "/" then  i want to show Home component otherwise localhost:3000/login will show home as well as login*/}
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
+        </div>
+      </Router>
     );
   }
 }
