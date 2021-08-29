ee32b44765b5b1c824a65f2df58b67bd2d87f681
Abhishek Verma <avishekv444@gmail.com>
2021-08-30 01:27:45 +0530

    user login 1

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,1 +1,6 @@
 export const UPDTAE_POSTS='UPDTAE_POSTS';
+
+//login action types
+export const LOGIN_START='LOGIN_START';
+export const LOGIN_FAILED='LOGIN_FAILED';
+export const LOGIN_SUCCESS='LOGIN_SUCCESS';
---
+++ src/reducers/auth.js
@@ -0,0 +1,35 @@
+import { LOGIN_FAILED, LOGIN_START, LOGIN_SUCCESS } from "../actions/actionTypes";
+
+const initialAuthState={
+    user:{},
+    error:null,
+    isLoggedIn:false,
+    inProgress:false
+}
+
+export default function auth(state=initialAuthState,action){
+    switch(action.type){
+        case LOGIN_START:
+            return{
+                ...state,
+                inProgress:true
+            };
+        case LOGIN_SUCCESS:  //login success should give jwt token as well as user object
+        return{
+            ...state,
+            user:action.user,
+            isLoggedIn:true,
+            inProgress:false,
+            error:null
+        };
+        case LOGIN_FAILED:
+            return{
+                ...state,
+                error:action.error,
+                inProgress:false
+            };
+       
+        default:
+            return state;
+    }
+}
