ed13314fe2028c2d83b6b5d01c1f6b7772f60a25
Abhishek Verma <avishekv444@gmail.com>
2021-09-01 17:50:03 +0530

    User Profie: handling route params

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Link, Redirect, Route, Switch } from 'react-router-dom'; //renaming BrowserRouter as Router for us
+import {
+  BrowserRouter as Router,
+  Link,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom'; //renaming BrowserRouter as Router for us
 import PropTypes from 'prop-types';
 import jwt_decode from 'jwt-decode';
 
 import { fetchPosts } from '../actions/posts';
-import { Home, Navbar, Page404, Login, Signup as SignUp ,Settings} from './'; //automatically importing from index.js
+import { Home, Navbar, Page404, Login, Signup as SignUp, Settings,UserProfile } from './'; //automatically importing from index.js
 import { authenticateUser } from '../actions/auth';
-import {getAuthTokenFromLocalStorage} from '../helpers/utils';
+import { getAuthTokenFromLocalStorage } from '../helpers/utils';
+
 
 //const Settings= () => <div> settings </div> dummy component
 
@@ -25,9 +32,10 @@ const PrivateRoute = (privateRouteProps) => {
           <Redirect
             to={{
               pathname: '/login',
-              state: {   //The state object can be accessed via this.props.location.state in the redirected-to component
-                from: props.location,  //this is from where the user is comming from
-                //2 doubts ---Q1-how state is going inside location 
+              state: {
+                //The state object can be accessed via this.props.location.state in the redirected-to component
+                from: props.location, //this is from where the user is comming from
+                //2 doubts ---Q1-how state is going inside location
                 //Q2- location has 4 variables how we are getting only pathname inside from
               },
             }}
@@ -43,20 +51,22 @@ class App extends React.Component {
     this.props.dispatch(fetchPosts());
 
     const token = getAuthTokenFromLocalStorage();
-     if (token) {
-       const user = jwt_decode(token);
-       console.log('user', user);
-       this.props.dispatch(authenticateUser({
-         name:user.name,
-         email:user.email,
-         _id:user._id
-       }))
-     }
+    if (token) {
+      const user = jwt_decode(token);
+      console.log('user', user);
+      this.props.dispatch(
+        authenticateUser({
+          name: user.name,
+          email: user.email,
+          _id: user._id,
+        })
+      );
+    }
   }
 
   //we have to wrap everything between router:-this will basically tell react router that hey this is our root application
   render() {
-    const { posts,auth } = this.props;
+    const { posts, auth } = this.props;
     return (
       <Router>
         <div>
@@ -73,7 +83,16 @@ class App extends React.Component {
             {/* when i want to pass props in Route tag than instead of using component i have to use render but because of this the default props like history that was recived by Home will not be recived so we have to send default props also which is recived by our callback from react */}
             <Route path="/login" component={Login} />
             <Route path="/signup" component={SignUp} />
-            <PrivateRoute path="/settings" component={Settings} isLoggedin={auth.isLoggedin} />
+            <PrivateRoute
+              path="/settings"
+              component={Settings}
+              isLoggedin={auth.isLoggedin}
+            />
+            <PrivateRoute
+              path="/user/:userId"  //by writing like this we can tell the react router that hey whatever is passed after user just give me that string or anything the user passes after the / into a varialble name userId
+              component={UserProfile}
+              isLoggedin={auth.isLoggedin}
+            />
             <Route component={Page404} />
           </Switch>
         </div>
@@ -85,7 +104,7 @@ class App extends React.Component {
 function mapStateToProps(state) {
   return {
     posts: state.posts,
-    auth:state.auth
+    auth: state.auth,
   };
 }
 
