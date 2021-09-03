0078db7cd234f3dd0f9ded1a72964d8f99c8fc0e
Abhishek Verma <avishekv444@gmail.com>
2021-09-03 16:43:53 +0530

    creating comments

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,4 +1,4 @@
-import {UPDTAE_POSTS, ADD_POST} from '../actions/actionTypes';
+import {UPDTAE_POSTS, ADD_POST,ADD_COMMENT} from '../actions/actionTypes';
 
 export default function posts (state=[],action){
     switch(action.type){
@@ -6,6 +6,18 @@ export default function posts (state=[],action){
             return action.posts; //this will be the new state
         case ADD_POST:
             return [action.post, ...state]; //returning array and adding newPost to the top and then the previous state
+        case ADD_COMMENT:
+            const newPosts = state.map((post) => {
+                if (post._id === action.postId) {
+                return {
+                    ...post,
+                    comments: [action.comment, ...post.comments],
+                };
+                }
+        
+                return post;
+            });
+            return newPosts;
         default:
             return state;
     }
