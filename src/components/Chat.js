24ae6404d650e93fce675cb41becd280357f4d9e
Abhishek Verma <avishekv444@gmail.com>
2021-09-07 14:38:19 +0530

    chat finished

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import io from 'socket.io-client';
 import '../chat.css';
 
 class Chat extends Component {
@@ -9,7 +11,60 @@ class Chat extends Component {
       messages: [], // this is the array of objects whose objects looks like----- {content: 'some message', self: true}
       typedMessage: '',
     };
+
+    this.socket = io.connect('http://codeial.codingninjas.com:5000/'); //connecting socket to the server ----akash's sir chat server
+    this.userEmail = props.user.email;
+
+    if (this.userEmail) {
+      this.setupConnections();
+    }
   }
+
+  setupConnections = () => {
+    const socketConnection = this.socket;
+    const self = this;
+
+    this.socket.on('connect', function () { //by using method 'on' we can define actions like here our defined action is connect and when the socket connection is finished sucessfully then this callback will be runned once.
+      console.log('CONNECTION ESTABLISHED');
+      socketConnection.emit('join_room', { //using emit we can send any action to the server here we are sending action to join room
+        user_email: this.userEmail,
+        chatroom: 'codeial',
+      });
+
+      socketConnection.on('user_joined', function (data) { //listening for user joined action and whenever a user joins the room we will get data in our callback
+        console.log('NE USER JOINED', data);
+      });
+    });
+
+    this.socket.on('receive_message', function (data) { //listener for receive message and then pushing that data into messages array
+      // add message to state
+      const { messages } = self.state;
+      const messageObject = {}; 
+      messageObject.content = data.message;
+
+      if (data.user_email === self.userEmail) {
+        messageObject.self = true;
+      }
+
+      self.setState({
+        messages: [...messages, messageObject],
+        typedMessage: '',
+      });
+    });
+  };
+
+  handleSubmit = () => {
+    const { typedMessage } = this.state;
+
+    if (typedMessage && this.userEmail) {
+      this.socket.emit('send_message', {
+        message: typedMessage,
+        user_email: this.userEmail,
+        chatroom: 'codeial',
+      });
+    }
+  };
+
   render() {
     const { typedMessage, messages } = this.state;
 
@@ -18,7 +73,7 @@ class Chat extends Component {
         <div className="chat-header">
           Chat
           <img
-            src="https://www.iconsdb.com/icons/preview/white/minus-5-xxl.png"
+            src="https://image.flaticon.com/icons/png/512/1828/1828906.png"
             alt=""
             height={17}
           />
@@ -49,4 +104,9 @@ class Chat extends Component {
   }
 }
 
-export default Chat;
+function mapStateToProps({ auth }) {
+  return {
+    user: auth.user,
+  };
+}
+export default connect(mapStateToProps)(Chat);
